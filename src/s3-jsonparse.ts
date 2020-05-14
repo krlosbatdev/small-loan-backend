3a32db48c0d9ea86db3956322afa413b3c4e6e29
Kar <krlosbatdev@gmail.com>
2020-05-14 17:16:17 -0400

    add parse json

--- src/s3-jsonparse.ts
+++ src/s3-jsonparse.ts
@@ -1,14 +1,35 @@
-import { DocumentProcessor } from "../libs/textract.lib";
 
+import { getJsonFromS3, getSanitizedKeyValues, addKeyValuesToDynamoDB } from '../libs/parse-json.event.lib';
 
-export async function main(event: any, context:any) {
+export async function main(event: any, context: any) {
+  // Logs starting message
+  console.log('send-textract-result-to-dynamo - start');
+  console.log(JSON.stringify(event, null, 4));
+  let bucket;
+  let key;
 
   try {
-
+    bucket = event['Records'][0]['s3']['bucket']['name'];
+    key = event['Records'][0]['s3']['object']['key'];
   } catch (e) {
+    console.log('Error parsing file info from event');
     console.log(e);
-    // console.log('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-    // raise e
+    return;
   }
 
+  console.log('bucket: ' + bucket);
+  console.log('key: ' + key);
+
+  let data = await getJsonFromS3({ bucket, key }).catch((err) => {
+    console.log('Error reading json file form s3');
+    console.log(err);
+    return;
+  });
+
+  const sanitizedKvPairs: { [key: string]: string } = getSanitizedKeyValues(data);
+
+  await addKeyValuesToDynamoDB(sanitizedKvPairs);
+
+  // Logs shutdown message
+  console.log('send-textract-result-to-dynamo - shutdown');
 }
